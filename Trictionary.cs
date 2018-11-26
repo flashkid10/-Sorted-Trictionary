7318e158f0cd21be20f8f87f7ff8a0bf934f680d
Jonathan Mathai <jonvmathai@gmail.com>
2018-11-25 22:03:18 -0500

    Update Trictionary.cs

--- Trictionary.cs
+++ Trictionary.cs
@@ -3,29 +3,33 @@ using System.Linq;
 
 namespace Project
 {
-    public class Trictionary<Key, Value, Control>
+    public class Trictionary<Key, Value, Control> : SUTrictionaryBase<Key, Value, Control>
     {
-        public Trictionary()
-        {
-        }
+        public Trictionary() => IsSorted(false);
 
         public Trictionary(Trictionary<Key, Value, Control> Trictionary)
         {
+            IsSorted(false);
             foreach (KeyValueTriple<Key, Value, Control> U in (Trictionary.MemberwiseClone() as Trictionary<Key, Value, Control>)) Add(U);
         }
+    }
 
-        #region GetEnumerator
+    public class SortedTrictionary<Key, Value, Control> : SUTrictionaryBase<Key, Value, Control>
+    {
+        public SortedTrictionary() => IsSorted(true);
 
+        public SortedTrictionary(SortedTrictionary<Key, Value, Control> Trictionary)
+        {
+            IsSorted(true);
+            foreach (KeyValueTriple<Key, Value, Control> U in (Trictionary.MemberwiseClone() as SortedTrictionary<Key, Value, Control>)) Add(U);
+        }
+    }
+
+    public class SUTrictionaryBase<Key, Value, Control>
+    {
         public int Count { get { return DataTrack.Count(); } }
         public List<Key> Keys { get { return DataTrack.Keys.ToList(); } }
-        private Dictionary<Key, KeyValueTriple<Key, Value, Control>> DataTrack = new Dictionary<Key, KeyValueTriple<Key, Value, Control>>();
-
-        public IEnumerator<KeyValueTriple<Key, Value, Control>> GetEnumerator()
-        {
-            foreach (KeyValueTriple<Key, Value, Control> val in DataTrack.Values) yield return val;
-        }
-
-        #endregion GetEnumerator
+        public IDictionary<Key, KeyValueTriple<Key, Value, Control>> DataTrack = new Dictionary<Key, KeyValueTriple<Key, Value, Control>>();
 
         #region Dictionary
 
@@ -63,24 +67,8 @@ namespace Project
         }
 
         #endregion Dictionary
-    }
-
-    public class SortedTrictionary<Key, Value, Control>
-    {
-        public SortedTrictionary()
-        {
-        }
-
-        public SortedTrictionary(SortedTrictionary<Key, Value, Control> Trictionary)
-        {
-            foreach (KeyValueTriple<Key, Value, Control> U in (Trictionary.MemberwiseClone() as SortedTrictionary<Key, Value, Control>)) Add(U);
-        }
 
         #region GetEnumerator
-
-        public int Count { get { return DataTrack.Count(); } }
-        public List<Key> Keys { get { return DataTrack.Keys.ToList(); } }
-        private SortedDictionary<Key, KeyValueTriple<Key, Value, Control>> DataTrack = new SortedDictionary<Key, KeyValueTriple<Key, Value, Control>>();
 
         public IEnumerator<KeyValueTriple<Key, Value, Control>> GetEnumerator()
         {
@@ -89,42 +77,22 @@ namespace Project
 
         #endregion GetEnumerator
 
-        #region SortedDictionary
-
-        public List<Value> Values { get { return ValueDictionary.Values.ToList(); } }
-        public SortedDictionary<Key, Value> ValueDictionary = new SortedDictionary<Key, Value>();
-
-        public List<Control> Controls { get { return ControlDictionary.Values.ToList(); } }
-        public SortedDictionary<Key, Control> ControlDictionary = new SortedDictionary<Key, Control>();
-
-        public void Add(Key key, Value value, Control control) => Add(new KeyValueTriple<Key, Value, Control>(key, value, control));
-
-        public void Add(KeyValueTriple<Key, Value, Control> KVT)
+        public KeyValueTriple<Key, Value, Control> GetAtIndex(int Index)
         {
-            DataTrack.Add(KVT.Key, KVT);
-            ValueDictionary.Add(KVT.Key, KVT.Value);
-            ControlDictionary.Add(KVT.Key, KVT.Control);
-        }
-
-        public void Remove(Key key)
-        {
-            DataTrack.Remove(key);
-            ValueDictionary.Remove(key);
-            ControlDictionary.Remove(key);
-        }
-
-        public KeyValueTriple<Key, Value, Control> this[Key key]
-        {
-            get { return DataTrack[key]; }
-            set
+            int pointer = 0;
+            foreach (KeyValueTriple<Key, Value, Control> val in DataTrack.Values)
             {
-                DataTrack[key] = value;
-                ValueDictionary[key] = value.Value;
-                ControlDictionary[key] = value.Control;
+                if (pointer == Index) return val;
+                pointer++;
             }
+            return null;
         }
 
-        #endregion SortedDictionary
+        internal void IsSorted(bool IS)
+        {
+            if (IS) DataTrack = new SortedDictionary<Key, KeyValueTriple<Key, Value, Control>>();
+            else DataTrack = new Dictionary<Key, KeyValueTriple<Key, Value, Control>>();
+        }
     }
 
     public partial class KeyValueTriple<key, value, control>
